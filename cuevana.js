d839c60554a6a3a5c03414b4cd7741cd3623eba6
youaregoldenhehe-prog <youaregoldenhehe@gmail.com>
2026-05-09 20:03:11 +0200

    Create cuevana.js

---
+++ cuevana.js
@@ -0,0 +1,11 @@
+{
+  "id": "cuevana-spabila",
+  "name": "Cuevana Spabila",
+  "version": "1.0.1",
+  "type": "stream",
+  "matches": ["movie", "series"],
+  "logic": {
+    "source": "https://cuevana.st",
+    "query": "/search?q={{title}}"
+  }
+}
